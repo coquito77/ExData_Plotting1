c8a175d00983e6ca9067c00800826a02e810be65
coquito77 <moises.e01@gmail.com>
2015-06-05 12:38:00 -0700

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,23 @@
+
+# download file changed https to http
+
+temp <- tempfile()
+download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+data <- read.table(unz(temp, "household_power_consumption.txt"), header=T, 
+	sep=";",
+	colClasses = c("character", "character", rep("numeric",7)),
+                   na = "?")
+unlink(temp)
+
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+DATE1 <- as.Date("2007-02-01")
+DATE2 <- as.Date("2007-02-02")
+
+subSetData <- data[data$Date >= DATE1 & data$Date <= DATE2,]
+attach(subSetData)
+
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(subSetData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
